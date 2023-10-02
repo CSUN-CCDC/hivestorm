c96e158ff0df82116a2d5e63a6a151d18a907d27
Arian-D <ArianXDehghani@gmail.com>
2023-10-01 23:34:27 -0700

    Switch over to `nix develop'
    Use "nix develop ." to have a bash shell with all the packages you
    specified there. We're most likely going to solely do things on x86_64
    but I used the flake-utils that make it run on all architectures

--- flake.nix
+++ flake.nix
@@ -3,18 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.inputs = {};
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixConfigurations = {
-      default = {
-        packages = [ 
-          nixpkgs.bash
-          nixpkgs.zellij
-          nixpkgs.fsearch 
-        ];
-      };
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in {
+              devShells.default = pkgs.mkShell {
+                buildInputs = with pkgs; [
+                  fsearch
+                  zellij
+                  bash 
+                ];
+              };
+            }
+      );
 }
